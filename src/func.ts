c453c8e34c3b43d37c66f326b071ad65f36d3913
Marius <marius@example.com>
2022-07-08 10:44:12 +0300

    func uzd

---
+++ src/func.ts
@@ -0,0 +1,7 @@
+// isiimportuojam people ir interface
+
+// 1. parasyti Fn kuri atspausdina visus vyrus
+
+// 1.1 parasyti Fn kuri  atspausdina moteris
+
+// 2. parasyti Fn kuri grazina masyve moteris jaunesnies nei 35 metai
