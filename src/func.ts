056b05855f4a12a86b1e18ca5d7f0f716c373860
Marius <marius@example.com>
2022-07-08 10:57:44 +0300

    uzd

--- src/func.ts
+++ src/func.ts
@@ -5,3 +5,11 @@
 // 1.1 parasyti Fn kuri  atspausdina moteris
 
 // 2. parasyti Fn kuri grazina masyve moteris jaunesnies nei 35 metai
+
+// 3. parasyti interface funkcijai ir Fn kuri grazina masyve visus žmones kurie turi mašinas
+
+// 4. parasyti fn atspausdinti visų vairuojančių žmonių lytis
+// 4.1 parasyti fn grazinti objektu masyva kuriame yra vardas ir lytis visų vairuojančių žmonių
+
+// 5. parasyti fn kuri suskaiciuoja ir grazina kiek yra vairuojanciu vyru ir moteru
+// pvz {man: 4, woman: 5}
